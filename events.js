0ff2aff6520b55d571cb1b8351cee27438d7ce2d
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-11-27 10:24:04 -0600

    feat: Implemented a dom connection between the html elements and the script.js file.

---
+++ events.js
@@ -0,0 +1,6 @@
+const eventNameInput = document.querySelector("#event-name");
+const representativeNameInput = document.querySelector("#representative-name");
+const representativeEmailInput = document.querySelector("#representative-email");
+const roleSelected = document.querySelector("#company-role");
+const submitButton = document.querySelector("#submit");
+
