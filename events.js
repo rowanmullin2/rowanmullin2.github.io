ff7a34f3fa5c7698d06f6a1fb9f8220cbf6ee8ed
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-11-27 10:32:46 -0600

    feat: added a function that checks and adds error messages to the input if a requirement is not met.

--- events.js
+++ events.js
@@ -19,3 +19,67 @@ function clearError () {
     })
     console.log("Errors cleared")
 }
+
+function isValidForm() {
+    let errs = [];
+    valid = true;
+
+    clearError();
+
+    // Event name
+    if (eventNameInput.value.trim() === "") {
+        console.log("Error: Event name is empty");
+        valid = false;
+        errs.push({
+            inputNode: eventNameInput,
+            message: "Please enter the Event name."
+        })
+    }
+
+    // Company's representative name
+    if (representativeNameInput.value.trim() === "") {
+        console.log("Error: Representative's name is empty");
+        valid = false;
+        errs.push({
+            inputNode: representativeNameInput,
+            message: "Please enter company's representative name."
+        })
+    }
+    
+    // Company's representative email
+    if (representativeEmailInput.value.trim() === "") {
+        console.log("Error: Representative's email is empty");
+        valid = false;
+        errs.push({
+            inputNode: representativeEmailInput,
+            message: "Please enter company's representative email."
+        })
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(representativeEmailInput.value.trim())) {
+        console.log("Error: Representative's email is invalid");
+        valid = false;
+        errs.push({
+            inputNode: representativeEmailInput,
+            message: "Please enter a valid email."
+        })
+    }
+
+    // Company's representative name
+    if (roleSelected.value.trim() === "") {
+        console.log("Error: Role selection is empty");
+        valid = false;
+        errs.push({
+            inputNode: roleSelected,
+            message: "Please enter company's role."
+        })
+    }
+
+    if (errs.length === 0) {
+        console.log("Form is valid");
+    }
+
+    errs.forEach((err) => {
+        showError(err.inputNode, err.message)
+    })
+
+    return valid
+}
