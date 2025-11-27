b6654cd0a11d66bb566a594ef3dec3f23c2fd8a5
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-11-27 10:26:05 -0600

    feat: Created a function for displaying the error shown in a container next to the parent element.

--- events.js
+++ events.js
@@ -4,3 +4,11 @@ const representativeEmailInput = document.querySelector("#representative-email")
 const roleSelected = document.querySelector("#company-role");
 const submitButton = document.querySelector("#submit");
 
+function showError(inputNode, message) {
+    let error = document.createElement("div");
+    error.classList.add('error');
+    error.innerHTML = message;
+    inputNode.parentNode.insertBefore(error, inputNode.nextSibling);
+    console.log('Error Displayed')
+}
+
