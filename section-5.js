1eaeb6a4d56b8224ce0e9a25a391d168920272bf
Rowan Mullin <rowanmullin@gmail.com>
2025-11-28 23:01:45 -0600

    setup queries and event listener for button

--- section-5.js
+++ section-5.js
@@ -1,5 +1,16 @@
-if (typeof window !== "undefined") {
-    window.onload = init;
-} 
+const nameInput = document.querySelector("#name-input")
+const emailInput = document.querySelector("#email-input")
+const donationInput = document.querySelector("#donation-input")
+const amountInput = document.querySelector("#amount-input")
+const commentsInput = document.querySelector("#comment-input")
+const submitButton = document.querySelector("#submit-button")
 
-module.exports = {};
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    buildObject()
+})
+
+
+if (typeof window === "undefined") {
+    module.exports = {buildObject};
+}
