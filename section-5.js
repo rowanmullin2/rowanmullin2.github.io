a3611fd20156f25e8234881ecfd6e3f515ec27ba
Rowan Mullin <rowanmullin@gmail.com>
2025-11-28 23:02:15 -0600

    created buildObject function for form submission

--- section-5.js
+++ section-5.js
@@ -5,6 +5,17 @@ const amountInput = document.querySelector("#amount-input")
 const commentsInput = document.querySelector("#comment-input")
 const submitButton = document.querySelector("#submit-button")
 
+function buildObject() {
+    let new_object = {
+        name:nameInput.value,
+        email:emailInput.value,
+        donation:donationInput.value,
+        amount:amountInput.value,
+        comment:commentsInput.value
+    }
+    console.log(new_object)
+}
+
 submitButton.addEventListener('click', (e) => {
     e.preventDefault()
     buildObject()
