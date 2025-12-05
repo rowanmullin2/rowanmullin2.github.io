ce6cb636ba29d2915e9b1a523d881fb9295ed353
Rowan Mullin <rowanmullin@gmail.com>
2025-12-04 23:43:35 -0600

    created updateTally function to track total donated items

--- section-5.js
+++ section-5.js
@@ -5,6 +5,7 @@ let amountInputElement;
 let commentsInputElement;
 let submitButtonElement;
 let tableElement;
+let tallyElement;
 
 function validateName(nameInput) {
     const nameStr = nameInput.value.trim();
@@ -71,108 +72,120 @@ function buildObject() {
         comment:commentsInputElement.value
     }
     if (document.querySelector(".err-msg")) {
-        console.log("invalid input")
-        return false
+        console.log("invalid input");
+        return false;
     } else {
         console.log(new_object);
-        return new_object
+        return new_object;
     }
 }
 
 function clearTable() {
-    tableElement.innerHTML = ""
+    tableElement.innerHTML = "";
 
-    let tableHead = document.createElement("tr")
-    tableElement.appendChild(tableHead)
+    let tableHead = document.createElement("tr");
+    tableElement.appendChild(tableHead);
 
-    let h1 = document.createElement("th")
-    h1.innerHTML = "Name"
-    tableHead.appendChild(h1)
+    let h1 = document.createElement("th");
+    h1.innerHTML = "Name";
+    tableHead.appendChild(h1);
     
-    let h2 = document.createElement("th")
-    h2.innerHTML = "Email"
-    tableHead.appendChild(h2)
+    let h2 = document.createElement("th");
+    h2.innerHTML = "Email";
+    tableHead.appendChild(h2);
 
     let h3 = document.createElement("th")
     h3.innerHTML = "Donation Item"
     tableHead.appendChild(h3)
     
-    let h4 = document.createElement("th")
-    h4.innerHTML = "Amount"
-    tableHead.appendChild(h4)
+    let h4 = document.createElement("th");
+    h4.innerHTML = "Amount";
+    tableHead.appendChild(h4);
     
-    let h5 = document.createElement("th")
-    h5.innerHTML = "Comment"
-    tableHead.appendChild(h5)
+    let h5 = document.createElement("th");
+    h5.innerHTML = "Comment";
+    tableHead.appendChild(h5);
 }
 
 function loadData() {
-    const data = localStorage.getItem("section-5")
-    return (!data) ? {} : JSON.parse(data)
+    const data = localStorage.getItem("section-5");
+    return (!data) ? {} : JSON.parse(data);
+}
+
+function updateTally() {
+    let data = loadData();
+    let tally = 0;
+
+    Object.values(data).forEach(entry => {
+        tally += entry.amount
+    })
+
+    tallyElement.innerHTML = `Total Amount of Items Donated: ${tally}`
 }
 
 function updateTable() {
     clearTable()
     Object.entries(loadData()).forEach((entry) => {
-        let index = entry[0]
-        let data = entry[1]
-        let tr = document.createElement("tr")
-        tableElement.appendChild(tr)
+        let index = entry[0];
+        let data = entry[1];
+        let tr = document.createElement("tr");
+        tableElement.appendChild(tr);
 
-        let nameDisplay = document.createElement("td")
-        nameDisplay.innerHTML = data.name
-        tr.appendChild(nameDisplay)
+        let nameDisplay = document.createElement("td");
+        nameDisplay.innerHTML = data.name;
+        tr.appendChild(nameDisplay);
 
-        let emailDisplay = document.createElement("td")
-        emailDisplay.innerHTML = data.email
-        tr.appendChild(emailDisplay)
+        let emailDisplay = document.createElement("td");
+        emailDisplay.innerHTML = data.email;
+        tr.appendChild(emailDisplay);
 
-        let donationDisplay = document.createElement("td")
-        donationDisplay.innerHTML = data.donation
-        tr.appendChild(donationDisplay)
+        let donationDisplay = document.createElement("td");
+        donationDisplay.innerHTML = data.donation;
+        tr.appendChild(donationDisplay);
 
-        let amountDisplay = document.createElement("td")
-        amountDisplay.innerHTML = data.amount
-        tr.appendChild(amountDisplay)
+        let amountDisplay = document.createElement("td");
+        amountDisplay.innerHTML = data.amount;
+        tr.appendChild(amountDisplay);
 
-        let commentDisplay = document.createElement("td")
-        commentDisplay.innerHTML = data.comment
-        tr.appendChild(commentDisplay)
+        let commentDisplay = document.createElement("td");
+        commentDisplay.innerHTML = data.comment;
+        tr.appendChild(commentDisplay);
 
-        let deleteButton = document.createElement("button")
-        deleteButton.setAttribute("type","button")
-        deleteButton.innerHTML = "X"
+        let deleteButton = document.createElement("button");
+        deleteButton.setAttribute("type","button");
+        deleteButton.innerHTML = "X";
 
         deleteButton.addEventListener("click", () => {
-            deleteEntry(index)
+            deleteEntry(index);
         })
 
-        tr.appendChild(deleteButton)
+        tr.appendChild(deleteButton);
     })
+    updateTally();
 }
 
 function deleteEntry(id) {
-    let data = loadData()
+    let data = loadData();
 
     Object.entries(data).forEach(entry => {
-        if (entry[0] >= id) {delete data[entry[0]]}
+        if (entry[0] >= id) {delete data[entry[0]];}
     })
 
     Object.entries(loadData()).forEach(entry => {
-        if (entry[0] > id) {data[entry[0]-1] = entry[1]}
+        if (entry[0] > id) {data[entry[0]-1] = entry[1];}
     })
 
-    localStorage.setItem("section-5",JSON.stringify(data))
-    updateTable()
+    localStorage.setItem("section-5",JSON.stringify(data));
+    updateTable();
 }
 
 function addInfo(newInfo) {
-    let data = loadData()
-    let entryCount = Object.keys(data).length
-    data[entryCount] = newInfo
+    let data = loadData();
+    let entryCount = Object.keys(data).length;
+    data[entryCount] = newInfo;
 
-    localStorage.setItem("section-5",JSON.stringify(data))
-    updateTable()
+    localStorage.setItem("section-5",JSON.stringify(data));
+    updateTable();
 }
 
 function init() {
@@ -182,7 +195,8 @@ function init() {
     amountInputElement = document.querySelector("#amount-input");
     commentsInputElement = document.querySelector("#comment-input");
     submitButtonElement = document.querySelector("#submit-button");
-    tableElement = document.querySelector("table")
+    tableElement = document.querySelector("table");
+    tallyElement = document.querySelector("#number-of-donations");
 
     updateTable()
 
@@ -190,7 +204,7 @@ function init() {
         e.preventDefault();
         let entry = buildObject();
         if (entry !== false) {
-            addInfo(entry)
+            addInfo(entry);
         }
     })
 }
@@ -198,5 +212,5 @@ function init() {
 if (typeof window === "undefined") {
     module.exports = {validateName, validateEmail, validateDonation, validateAmount, clearErrors, displayError, buildObject, init};
 } else {
-    init()
+    init();
 }
